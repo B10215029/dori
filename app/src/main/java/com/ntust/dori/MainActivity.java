0780fbbe1d2dccdee944cce320793a67d8d36063
b10215029 <b10215029@mail.ntust.edu.tw>
2016-01-08 20:29:10 +0800

    modify ui and some code

--- app/src/main/java/com/ntust/dori/MainActivity.java
+++ app/src/main/java/com/ntust/dori/MainActivity.java
@@ -3,7 +3,6 @@ package com.ntust.dori;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
-import android.net.Uri;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
@@ -17,7 +16,7 @@ import java.util.List;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private static final int REQUEST_CODE = 10;
+    private static final int SPEECH_REQUEST_CODE = 10;
     Button speak;
     ListView wordList;
 
@@ -32,40 +31,42 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-//                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-//                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
                 String lang = Locale.TRADITIONAL_CHINESE.toString();
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, lang);
                 intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, lang);
                 intent.putExtra(RecognizerIntent.EXTRA_ONLY_RETURN_LANGUAGE_PREFERENCE, lang);
                 intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "OK Dori");
-                startActivityForResult(intent, REQUEST_CODE);
+                startActivityForResult(intent, SPEECH_REQUEST_CODE);
             }
         });
 
-        PackageManager pm = getPackageManager();
-        List<ResolveInfo> activities = pm.queryIntentActivities(
-                new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
-
+        PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
         if (activities.size() == 0) {
             speak.setEnabled(false);
             speak.setText("no intent");
         }
 
-        Intent sendIntent = new Intent(Intent.ACTION_PICK);
-//        sendIntent.setAction(Intent.ACTION_SEND);
-//        sendIntent.putExtra(Intent.EXTRA_TEXT, "Your Message Here");
-//        sendIntent.setType("text/plain");
-//        sendIntent.setPackage("com.google.android.apps.maps");
-        startActivity(sendIntent);
+        ((Button) findViewById(R.id.test_button_1)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ((Button) findViewById(R.id.test_button_2)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
             ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-            wordList.setAdapter(
-                    new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, matches));
+            wordList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, matches));
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
