5ddace679e80888f97c11ad4f84c960633a126b1
b10215029 <b10215029@mail.ntust.edu.tw>
2016-01-09 15:33:56 +0800

    新增指令與按鈕 增加多種指令 新增輸入指令UI

--- app/src/main/java/com/ntust/dori/MainActivity.java
+++ app/src/main/java/com/ntust/dori/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -68,6 +69,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        ((Button) findViewById(R.id.button_run)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<String> matches = new ArrayList<String>();
+                matches.add(((EditText) findViewById(R.id.editText1)).getText().toString());
+                matches.add(((EditText) findViewById(R.id.editText2)).getText().toString());
+                //exec(analyze(matches));
+                exec(matches);
+            }
+        });
+
         db = openOrCreateDatabase("instruction", MODE_PRIVATE, null);
         db.execSQL("CREATE TABLE IF NOT EXISTS Hotkey(_id INTEGER PRIMARY KEY, alias TEXT, instruction TEXT UNIQUE)");
     }
@@ -101,7 +113,82 @@ public class MainActivity extends AppCompatActivity {
 
     void exec(ArrayList<String> instruction) {
         if (instruction.get(0).equals("打開相機")) {
-            startActivityForResult(new Intent("android.media.action.IMAGE_CAPTURE"), CAMERA_REQUEST_CODE);
+            Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+            startActivity(intent);
+        }
+        else if (instruction.get(0).equals("打開聯絡人")) {
+            Uri contacts = Uri.parse("content://contacts/people");
+            Intent showContacts = new Intent(Intent.ACTION_VIEW, contacts);
+            startActivity(showContacts);
+        }
+        else if (instruction.get(0).equals("寄信給")) {
+            Uri mail = Uri.parse("mailto:" + instruction.get(1));
+            Intent sendEmail = new Intent(Intent.ACTION_SENDTO, mail);
+            startActivity(sendEmail);
+        }
+        else if (instruction.get(0).equals("撥給")) {
+            Uri uri = Uri.parse("tel:" + instruction.get(1));
+            Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+            startActivity(intent);
+        }
+        else if (instruction.get(0).equals("撥出")) {
+            Uri uri = Uri.parse("tel:" + instruction.get(1));
+            Intent intent = new Intent(Intent.ACTION_CALL, uri);
+            startActivity(intent);
+        }
+        else if (instruction.get(0).equals("簡訊")) {
+            Uri uri = Uri.parse("smsto:" + instruction.get(1));
+            Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+//            intent.putExtra("sms_body", "The SMS text");
+            startActivity(intent);
+        }
+        else if (instruction.get(0).equals("連到")) {
+            Uri uri = Uri.parse("http://" + instruction.get(1));
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        }
+        else if (instruction.get(0).equals("執行")) {
+            Intent launchApp = getPackageManager().getLaunchIntentForPackage(instruction.get(1));
+            if (launchApp != null) {
+                launchApp.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(launchApp);
+            } else {
+                Uri uri = Uri.parse("market://details?id=" + instruction.get(1));
+                launchApp = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(launchApp);
+            }
+        }
+        else if (instruction.get(0).equals("選擇")) {
+            Intent selApp = new Intent(Intent.ACTION_MAIN);
+            selApp.addCategory(Intent.CATEGORY_LAUNCHER);
+            startActivity(Intent.createChooser(selApp, "Choose an App to launch"));
+        }
+        else if (instruction.get(0).equals("檔案")) {
+            Intent pickFile = new Intent(Intent.ACTION_GET_CONTENT);
+            pickFile.setType("*/*");
+            pickFile.addCategory(Intent.CATEGORY_OPENABLE);
+            startActivity(Intent.createChooser(pickFile, "selecta File from content provider"));
+        }
+        else if (instruction.get(0).equals("搜尋")) {
+            Uri uri = Uri.parse("http://www.google.com/search?q=" + instruction.get(1));
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        }
+        else if (instruction.get(0).equals("位置")) {
+            Uri uri = Uri.parse("geo:0,0?q=" + instruction.get(1));
+            Intent it = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(it);
+        }
+        else if (instruction.get(0).equals("打開youtube")) {
+            Intent launchApp = getPackageManager().getLaunchIntentForPackage("com.google.android.youtube");
+            if (launchApp != null) {
+                launchApp.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(launchApp);
+            } else {
+                Uri uri = Uri.parse("market://details?id=" + "com.google.android.youtube");
+                launchApp = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(launchApp);
+            }
         }
     }
 }
